64b99df4f2598951c0481ed188d3fb8c964c42ab
FredT93600 <ftetard@asso-auxilia.fr>
2023-01-04 10:42:23 +0100

    message

---
+++ asset/app.js
@@ -0,0 +1,6 @@
+// je declare ma variable
+let message = "je suis lié à mon fichier html";
+let age = 40;
+
+// j'execute mune fonction avec comme parametre la variable que j'ai definie plus haut
+console.log(message, age);
